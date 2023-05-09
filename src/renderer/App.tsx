c997794f1c1fb7688346e8306bb3997bb1f389cd
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-05-09 16:47:23 -0400

    added ICP messaging with Electron

---
+++ src/renderer/App.tsx
@@ -0,0 +1,49 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import RestCard from './components/RestCard';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const sendMessage =() => {
+  window.electron.terminalMessage(" User clicked h1.")
+}
+
+function Hello() {
+  const [restList, setRestList] = useState<any>();
+  useEffect(() => {
+    fetch('https://my-first-firestore-bc.web.app/restaurants')
+      .then((resp) => resp.json())
+      .then(setRestList)
+      .catch(alert)
+  }, []);
+  return (
+    <div className='App'>
+      <div className="Hello" onClick={sendMessage}>
+        <h1>Electron Restaurant List</h1>
+      </div>
+      <div className="Hello">
+        {! restList
+          ? <h2>Loading</h2>
+          : <div className='rest-list'>
+            {restList.map (rest => (
+              <RestCard rest={rest} key={rest.id} />
+        ))}
+        </div>}
+      </div>
+    </div>
+  );
+}
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+
+
+
+
